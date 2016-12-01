a79150c2130df13094208c7c81182d4bfa3a4f1f
suvradip saha <suvradip@fusioncharts.com>
2016-12-01 18:06:44 +0530

    - new template show data included

--- server.js
+++ server.js
@@ -20,10 +20,18 @@ app.use('/bower_components', express.static('bower_components'));
 //angular app directory mapping
 app.use('/webapp', express.static('webapp'));
 
+//review page
 app.use('/api/review', require(__dirname+'/controllers/api/reviews'));
+//show data page
+app.use('/api/showdata', require(__dirname+'/controllers/api/showdata'));
 //if port number is changing, also change in gulpfile for browsersync proxy
 app.listen(3300, function(){ console.log('[server.js] Running on port :33000'); });
 
 app.get('/', function(req, res){
 	res.render('index');
 });
+
+app.get('/showdata', function(req, res){
+	res.render('showdata');
+});
+
