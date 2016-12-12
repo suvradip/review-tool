7591992353fd3019faf1c214b9cbaae8bb531fed
suvradip saha <suvradip@fusioncharts.com>
2016-12-12 12:21:46 +0530

    - changes

--- dbconnection.js
+++ dbconnection.js
@@ -1,14 +1,17 @@
 var mongoose,
-	config = require(__dirname+"/config"),
-	mongo;
+    config = require(__dirname+"/config"),
+    mongo;
 
 mongoose = require("mongoose");
 
 mongo = config.mongodb;
-mongoose.connect("mongodb://" + (mongo.usr ? mongo.usr + ':'+mongo.pwd : '') + mongo.constr);
+
+//console.log("mongodb://" + (mongo.usr ? mongo.usr + ':'+mongo.pwd+'@' : '') + mongo.constr, mongo.options);
+
+mongoose.connect("mongodb://" + (mongo.usr ? mongo.usr + ':'+mongo.pwd+'@' : '') + mongo.constr, mongo.options);
 mongoose.Promise = global.Promise;
 
-mongoose.connection.on("error", function(){console.log("[dbconnection.js] Mongodb: ERROR.");});
+mongoose.connection.on("error", function(err){console.log("[dbconnection.js] Mongodb: ERROR."+err);});
 mongoose.connection.once("open", function(){console.log("[dbconnection.js] Mongodb: Connected.");});
 
 module.exports = mongoose;
